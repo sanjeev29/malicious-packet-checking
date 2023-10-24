12f6aa5a55a471edccf0fb685a8aa546dd3a063b
Mia-Y-Kelly <miayn24@gmail.com>
2023-10-23 22:31:36 -0400

    Added more RBF functionality; general improvements
    * Added user input validation in `main()`. Uses argv[2] to create outfile in the results folder. Currently not adding actual RBF to outfile.
    * RBF is now a class.
    * Created H(.) function for choosing index. Used H(.) for RBF
      initialization.
    * Added the pseudorandom generation of 10,000 IP's using a HashMap. By
      using the IP addresses as keys, verifying the new IP is unique is
    built-in.
    * For debugging purposes, all the IP addresses are written to a file
      called `IPGen.txt`.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,66 +1,162 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
 #include <bitset>
+#include <map>
 #include <stdbool.h>
 
 #include "sha256.h"
  
 using std::string;
 using std::cout;
+using std::fstream;
 using std::cin;
 using std::endl;
-using std:: stoi;
+using std::stoi;
+using std::map;
 
 const bool DEBUG = true; // Used for debugging
 
 class RBF {
     public:
         int m;
-        int RBF_arr[];
+        std::vector<int> RBFGen;
+        map<string, int> bad_IP_Map;
+
         
         // Constructor
         RBF(int user_input) {
             m = user_input;
-            // Initialze RBF to all zeros
-            for(int i = 0; i < m; i++) {
-                RBF_arr[i] = 0;
-            }            
+            // Resize vector
+            RBFGen.resize(m);
+            
+            // Initialize RBF
+            for(int j = 0; j < m; j++) {
+                // Calculate chosen index using H(j)
+                int chosen;
+                chosen = H(std::to_string(j));
+
+                // RBF[H(j)]
+                RBFGen.at(chosen) = 0;
+
+                // RBF[1 - H(j)], check bounds to make sure it does not throw an error
+                if(chosen > 0)
+                    RBFGen.at(1 - chosen) = 1;
+            }
+
+            
+            // Print array after insertion
+            if(DEBUG) {
+                for(int i = 0; i < RBFGen.capacity() - 1; i++) {
+                    cout << RBFGen.at(i);
+                }
+                cout<<endl;
+            }
+            
+
+            // USED FOR DEBUGGING -- print all IP addresses to IPGen.txt
+            fstream ip_debug;
+            ip_debug.open("IPGen.txt", std::ios::out | std::ofstream::trunc); // Open new file or wipe existing file to overwrite
+            
+            
+            // Variables needed for generating "random" IP's
+            string ip = "192.168.";
+            int w, x, y, z = 0;         // Used for randomly generating each of the spots  
+            string full_ip = "";        // Store 192.168.w.xyz in here
+            bool inserted = false;
+            bool temp = false;
+
+            // Embed 10,000 IP Addresses in the RBF
+            for(int i = 0; i < 10000; i++) {
+                // Use a HashMap property that all keys must be unique to add a new "random" IP that was not already added
+                while(!inserted) {
+                    // Randomize the entry
+                    w = rand() % 10;
+                    x = rand() % 10;
+                    y = rand() % 10;
+                    z = rand() % 10;
+
+                    // Concatenate the next couple places
+                    full_ip = ip  + std::to_string(w) + "." + std::to_string(x) + std::to_string(y) + std::to_string(z);
+
+                    // Try to insert into HashMap
+                    // If true, it has successfully generated a new "random" IP to be inserted into the RBF
+                    if((temp = bad_IP_Map.insert({full_ip, i}).second)) {
+                        // INSERT IP INTO HASH MAP HERE
+                
+                        ip_debug << full_ip << endl;    // DEBUG: write to IPGen.txt
+                        inserted = temp;                // Break loop
+                    }
+                    full_ip.clear();
+                }
+
+                // Reset
+                inserted = false;
+                temp = false;
+                full_ip.clear();           
+            }
+            ip_debug.close();           
         }
-
-    /**
-     * h1 is one of 8 hash functions that will produce the indexes for the RBF.
-     * @param key 1 through 8
-     * @param input "Indeed, the “input” should be a column number in string format" (on assignment description)
-     * @param m needed for modulus calculation; function needs to be refactored I think
-     * @return index
-    */
-    // In theory, this would work. In reality, I have not tested it much yet
-    int h1(string key, string input) {
-        int index = -1; // If it returns -1, we know the hash failed.
-
-        // Perform key||input, || indicates concatentation
-        string input_cat = key + input;
-
-        // Begin SHA256(key||ip) (mod m)
-        string sha_no_trunc = sha256(input_cat);
         
-        // Truncate
-        string trunc_output_str = sha_no_trunc.substr(sha_no_trunc.length() - 5);
-        int trunc_output_int = stoi(trunc_output_str, 0, 16);
 
-        // Modulus
-        index = trunc_output_int % m;
+        /**
+         * H(.) to determine chosen; specific implementation is sha256("0"||input) mod 2.
+         * @param index
+         * @return Success: 1 or 0, Fail: -1
+        */
+        int H(string input) {
+            int val = -1;
 
-        if(DEBUG) {
-            cout << "size: " << sha_no_trunc.length() << endl;
-            cout << "sha256('"<< input << "'):" << sha_no_trunc << endl;
-            cout << "trunc_val: " << trunc_output_str.length() << " " << trunc_output_str << endl;
-            cout << "int: " << trunc_output_int << endl;
-            cout << "index: " << index << endl;
+            // Perform key||input, || indicates concatentation
+            string input_cat = "key" + input;
+
+            // Begin SHA256(key||ip) (mod m)
+            string sha_no_trunc = sha256(input_cat);
+            
+            // Truncate
+            string trunc_output_str = sha_no_trunc.substr(sha_no_trunc.length() - 5);
+            int trunc_output_int = stoi(trunc_output_str, 0, 16);
+
+            // Modulus
+            val = trunc_output_int % 2;
+
+            return val;
         }
 
-        return index;   
-    }
+        /**
+         * Keyed hash function. In theory, it should work. In reality, it has not been tested heavily.
+         * @param key 1 through 8
+         * @param input "Indeed, the “input” should be a column number in string format" (on assignment description)
+         * @param m needed for modulus calculation; function needs to be refactored I think
+         * @return index
+        */
+        int h_key(string key, string input) {
+            int index = -1; // If it returns -1, we know the hash failed.
+
+            // Perform key||input, || indicates concatentation
+            string input_cat = key + input;
+
+            // Begin SHA256(key||ip) (mod m)
+            string sha_no_trunc = sha256(input_cat);
+            
+            // Truncate
+            string trunc_output_str = sha_no_trunc.substr(sha_no_trunc.length() - 5);
+            int trunc_output_int = stoi(trunc_output_str, 0, 16);
+
+            // Modulus
+            index = trunc_output_int % m;
+
+            if(DEBUG) {
+                cout << "size: " << sha_no_trunc.length() << endl;
+                cout << "sha256('"<< input << "'):" << sha_no_trunc << endl;
+                cout << "trunc_val: " << trunc_output_str.length() << " " << trunc_output_str << endl;
+                cout << "int: " << trunc_output_int << endl;
+                cout << "index: " << index << endl;
+            }
+
+            return index;   
+        }
 };
 
 /**
@@ -87,16 +183,32 @@ int main(int argc, char *argv[])
     string m = argv[1];
     int RBF_init_val;
 
-    // Catch if user entered invalid input
+    // Validate user input for m
     try {
         RBF_init_val = stoi(m);
-    } 
-    catch(...) {
+    } catch(...) {
         cout << "ERROR: Cannot convert " << m << " to an integer" << endl;
         return -1;
     }
-        
+
+    // Validate user input for output file name
+    string filename = argv[2];
+    try {
+        if(filename.find(".txt") == std::string::npos)
+            throw(filename);
+    } catch (string bad_filename) {
+        cout << "ERROR: Must provide valid output file name" << endl;
+        return -1;
+    }
+
+    // Create RBF
     RBF RBFGen(RBF_init_val);
+
+    // Write RBF out outfile in Results folder
+    fstream output;
+    output.open("Results/"+ filename+".txt", std::ios::out | std::ios::trunc);
+    output << "Eventually we will put our RBF here :)\n";
+    output.close();
 
     return 0;
 }
