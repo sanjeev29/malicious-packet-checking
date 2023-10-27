14aa4af21845a68403a94ea9279a296873be88be
sanjeev40 <sanjeevraichur29@gmail.com>
2023-10-26 22:38:58 -0400

    Add IPCheck file

--- Source/IPCheck.cpp
+++ Source/IPCheck.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+#include <stdio.h>
 
 #include "sha256.h"
+#include "RBFGen.h"
  
 using std::string;
 using std::cout;
 using std::endl;
 using std:: stoi;
 
-int main(int argc, char *argv[])
-{
-    return 0;
+bool check_ips(RBF &curr_RBF, string IP) {
+    for (int i = 0; i < 9; i++) {
+        int h_i = curr_RBF.H_i(to_string(i), IP);
+        int H = curr_RBF.H(to_string(h_i));
+
+        if(H == 0 && curr_RBF.RBFGen[0][h_i] != 1) {
+            return false;
+        } else if (H == 1 && curr_RBF.RBFGen[0][h_i] == 1) {
+            return false;
+        }
+    }
+
+    return true;
 }
